9160170cf750bf7c789e1ed25355ec1eaa7a8488
Johnatan Lima <joh23nathan@gmail.com>
2019-09-03 21:08:45 -0300

    Adicionados libs, config e conexao

---
+++ Data/DespesasContexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoDespesas.Data
+{
+    public class DespesasContexto : DbContext
+    {
+        public DespesasContexto(DbContextOptions<DespesasContexto> options) : base (options)
+        {
+            
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ProjetoDespesas.Data;
 
 namespace ProjetoDespesas
 {
@@ -33,6 +35,8 @@ namespace ProjetoDespesas
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<DespesasContexto>(options => options.UseMySql(Configuration.GetConnectionString("myConn")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
