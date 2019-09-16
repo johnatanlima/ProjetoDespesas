042ec0a5cafb48db61d89afb873d92858d2967f4
noteDexter <joh23nathan@gmail.com>
2019-09-16 19:28:47 -0300

    Alteração de Rota

--- Startup.cs
+++ Startup.cs
@@ -61,7 +61,7 @@ namespace ProjetoDespesas
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=TipoDespesa}/{action=Index}/{id?}");
             });
         }
     }
